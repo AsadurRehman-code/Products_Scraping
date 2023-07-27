7c4eaa2250b2a4470b06e5787de884fa46e11a4c
Asad ur Rehman <asadsiraj.20@gmail.com>
2023-07-28 00:58:59 +0500

    Delete app.py

--- app.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-from product_scrape import main  # Assuming the product_scrape.py file is named "product_scrape.py"
-
-# Set a title for the Streamlit app
-st.title("Product Scrape App")
-
-# Run the main function from product_scrape.py
-main()
